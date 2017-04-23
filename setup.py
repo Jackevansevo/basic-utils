bc00655c25cdb81152ce5fe35fffc5e29d276b92
Jack Evans <jack@evans.gb.net>
2017-04-23 22:34:09 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='basic_utils',
-    version='0.2.0',
+    version='0.3.0',
     description='A simple set of Python utils',
     long_description=long_description,
     url='https://github.com/Jackevansevo/basic-utils',
