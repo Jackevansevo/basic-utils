85d6ca3a96bd80f64ba3c3603a2446827e3854df
Jack Evans <jwe@tiger-computing.co.uk>
2017-04-24 16:09:47 +0100

    added tests for compose and complement

--- tests/test_primitives.py
+++ tests/test_primitives.py
@@ -1,8 +1,10 @@
 import pytest
 
 from basic_utils.primitives import (
-    dec, even, identity, inc, odd
+    complement, compose, dec, even, identity, inc, odd
 )
+
+from basic_utils.helpers import rpartial
 
 
 @pytest.mark.parametrize("data", [
@@ -11,6 +13,18 @@ from basic_utils.primitives import (
 def test_identity(data):
     """Tests that identity returns the same arguments as it was passed"""
     assert identity(data) == data
+
+
+def test_compose():
+    assert compose(complement, even)(3)
+    assert compose(complement, odd)(2)
+
+
+def test_complement():
+    fn = complement(even)
+    assert fn(5)
+    fn = complement(lambda x: x.startswith('a'))
+    assert fn('grapple')
 
 
 def test_primitives():
