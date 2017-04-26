3468f91cff81d93607dfac880a4f804a67a52925
Jack Evans <jwe@tiger-computing.co.uk>
2017-04-26 12:21:00 +0100

    Added date util functions

--- basic_utils/__init__.py
+++ basic_utils/__init__.py
@@ -2,3 +2,4 @@ from .core import *  # noqa
 from .datastructures import *  # noqa
 from .helpers import *  # noqa
 from .primitives import *  # noqa
+from .dates import *  # noqa
---
+++ tests/test_dates.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from itertools import starmap
+
+from basic_utils.dates import dates_between, date_ranges_overlap
+
+
+def test_dates_between():
+    start = datetime(2016, 1, 1)
+    end = datetime(2016, 1, 3)
+    expected = starmap(datetime, ((2016, 1, 1), (2016, 1, 2), (2016, 1, 3)))
+    assert tuple(dates_between(start, end)) == tuple(expected)
+
+
+def test_ranges_overlap():
+    rangeX = (datetime(2012, 1, 15), datetime(2012, 5, 10))
+    rangeY = (datetime(2012, 3, 20), datetime(2012, 9, 15))
+    assert date_ranges_overlap(rangeX, rangeY)
+
+    rangeX = (datetime(2012, 1, 15), datetime(2012, 5, 10))
+    rangeY = (datetime(2015, 3, 20), datetime(2015, 9, 15))
+    assert not date_ranges_overlap(rangeX, rangeY)
