d2682da8fc26dd225bead01df20edb3ca45b1132
Jack Evans <jwe@tiger-computing.co.uk>
2017-04-28 12:11:39 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,5 +1,33 @@
-pyenv: autopep8: command not found
+# Always prefer setuptools over distutils
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-The `autopep8' command exists in these Python versions:
-  3.6.1
+here = path.abspath(path.dirname(__file__))
 
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='basic_utils',
+    version='1.5.1',
+    description='A simple set of Python utils',
+    long_description=long_description,
+    url='https://github.com/Jackevansevo/basic-utils',
+    author='Jack Evans',
+    author_email='jack@evans.gb.net',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='utils utilities',
+    packages=['basic_utils'],
+)
