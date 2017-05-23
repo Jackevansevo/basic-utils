e6f1c0591580b952947eaeeec57b78d1aa5341e4
Jack Evans <jack@evans.gb.net>
2017-05-23 21:34:22 +0100

    Clean up setup.py
    Seperate out dependencies. Added pytest-runner

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
-# Always prefer setuptools over distutils
-from setuptools import setup
-# To use a consistent encoding
 from codecs import open
+from itertools import chain
 from os import path
+
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+]
+
+dev_requirements = [
+    'flake8',
+    'isort',
+    'mypy',
+    'sphinx',
+    'sphinx-autobuild',
+    'sphinx_autodoc_typehints',
+    'sphinx_rtd_theme',
+]
 
 setup(
     name='basic_utils',
@@ -20,13 +36,19 @@ setup(
     author_email='jack@evans.gb.net',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Utilities',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
     ],
     keywords='utils utilities',
     packages=['basic_utils'],
+    setup_requires=['pytest-runner'],
+    tests_require=test_requirements,
+    extras_require={
+        'dev': list(chain(dev_requirements, test_requirements)),
+        'test': test_requirements,
+    }
 )
