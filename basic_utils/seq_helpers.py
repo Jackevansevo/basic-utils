5a7c7215e8d5329ac50b41e5446d8c6c915e6686
Jackevansevo <jack@evans.gb.net>
2017-06-13 22:20:10 +0100

    added additional seq functions

--- basic_utils/seq_helpers.py
+++ basic_utils/seq_helpers.py
@@ -94,13 +94,10 @@ def cons(item: Any, seq: Sequence) -> chain:
     return chain([item], seq)
 
 
-def flatten(seq: Iterable) -> Iterable:
+def lazy_flatten(seq: Iterable) -> Iterable:
     """
-    Returns a generator object which when evalutated
-    returns a flatted version of seq
-
-    >>> list(flatten([1, [2, [3, [4, 5], 6], 7]]))
-    [1, 2, 3, 4, 5, 6, 7]
+    Returns a generator which yields items from a flattened version
+    of the sequence.
     """
     for item in seq:
         if isinstance(item, Iterable) and not isinstance(item, (str, bytes)):
@@ -109,22 +106,29 @@ def flatten(seq: Iterable) -> Iterable:
             yield item
 
 
+def flatten(seq: Iterable) -> Iterable:
+    """ Returns a flatten version of sequence.
+
+    >>> flatten([1, [2, [3, [4, 5], 6], 7]])
+    [1, 2, 3, 4, 5, 6, 7]
+    """
+    return type(seq)(lazy_flatten(seq))  # type: ignore
+
+
 def partial_flatten(seq: Iterable) -> Iterable:
     """
-    Returns partially flattened version of seq
+    Returns partially flattened version of sequence.
 
-    >>> list(flatten([[1, 2, 3], [4, 5, 6]]))
-    [1, 2, 3, 4, 5, 6]
+    >>> partial_flatten(((1,), [2, 3], (4, [5, 6])))
+    (1, 2, 3, 4, [5, 6])
     """
-    return chain.from_iterable(seq)
+    return type(seq)(reduce(concat, seq))  # type: ignore
 
 
-def dedupe(seq: Sequence, key: Callable=None) -> Iterable:
+def lazy_dedupe(seq: Sequence, key: Callable=None) -> Iterable:
     """
-    Removes duplicates from a sequence while maintaining order
-
-    >>> list(dedupe([1, 5, 2, 1, 9, 1, 5, 10]))
-    [1, 5, 2, 9, 10]
+    Returns a generator which which yields items in the sequence skipping
+    duplicates.
     """
     seen = set()  # type: set
     for item in seq:
@@ -134,8 +138,82 @@ def dedupe(seq: Sequence, key: Callable=None) -> Iterable:
             seen.add(val)
 
 
+def sorted_index(seq: Sequence, item: Any, key: str=None) -> int:
+    """
+    >>> sorted_index([10, 20, 30, 40, 50], 35)
+    3
+    """
+    keyfn = itemgetter(key) if key is not None else None
+    cp = sorted(cons(item, seq), key=keyfn)
+    return cp.index(item)
+
+
+def dedupe(seq: Sequence, key: Callable=None) -> Iterable:
+    """
+    Removes duplicates from a sequence while maintaining order
+
+    >>> dedupe([1, 5, 2, 1, 9, 1, 5, 10])
+    [1, 5, 2, 9, 10]
+    """
+    return type(seq)(lazy_dedupe(seq, key))  # type: ignore
+
+
+def concat(seqX: Sequence, seqY: Sequence) -> Sequence:
+    """
+    Joins two sequences together, returning a single combined sequence.
+    Preserves the type of passed arguments.
+
+    >>> concat((1, 2, 3), (4, 5, 6))
+    (1, 2, 3, 4, 5, 6)
+    """
+    chained = chain(seqX, seqY)
+    if type(seqX) == type(seqY):
+        return type(seqX)(chained)  # type: ignore
+    return list(chained)
+
+
+def take(n: int, iterable: Iterable) -> Iterable:
+    """
+    Return first n items of the iterable as a list.
+
+    >>> take(2, range(1, 10))
+    [1, 2]
+    """
+    return list(islice(iterable, n))
+
+
+def nth(iterable: Iterable, n: int, default: Any=None) -> Any:
+    """
+    Returns the nth item or a default value.
+
+    >>> nth([1, 2, 3], 1)
+    2
+    """
+    return next(islice(iterable, n, None), default)
+
+
+def all_equal(iterable: Iterable) -> bool:
+    """
+    Returns True if all the elements are equal to each other.
+
+    >>> all_equal([True, True])
+    True
+    """
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)  # type: ignore
+
+
+def quantify(iterable: Iterable, pred: Callable=bool) -> int:
+    """
+    Returns count of how many times the predicate is true.
+
+    >>> quantify([True, False, True])
+    2
+    """
+    return sum(map(pred, iterable))
+
+
 # Define some common aliases
 head = first
 tail = rest
 init = butlast
-concat = chain
